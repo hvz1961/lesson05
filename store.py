6edabe745aebad1f8981ee7435cab4d19f5f1fff
hvz1961 <vzkhametov@gmail.com>
2024-10-18 16:31:26 +0300

    Update store.py

--- store.py
+++ store.py
@@ -3,18 +3,27 @@ class Store():
         self.name_store = name_store
         self.address = address
         self.items = {}
+
     def add_items(self, name, price):
         self.items[name] = price
+        print(f'товар {name} добавлен в {self.name_store}')
+
     def delete_items(self, name):
         if name in self.items:
             del self.items[name]
+            print(f'товар {name} удален из {self.name_store}')
+
     def get_price(self, name):
         return self.items.get(name, None)
+
     def update_price(self, name, new_price):
         if name in self.items:
             self.items[name] = new_price
+            print(f'цена на {name} изменена в {self.name_store}')
+        else:
+            print(f'товар {name} не найден')
 
-stor1 = Store('"Дикси"','Санкт-Петербург,ул.Милионная,д.3')
+stor1 = Store('"Дикси"','Санкт-Петербург ул.Милионная д.3')
 print(stor1.name_store,stor1.address)
 stor1.add_items('яблоки', 100)
 stor1.add_items('бананы', 50)
@@ -26,7 +35,7 @@ print(stor1.get_price('бананы'))
 stor1.update_price('яблоки', 120)
 print(stor1.get_price('яблоки'))
 
-stor2 = Store('"СтройДом"','Санкт-Петербург,ул.Войнова,д.15')
+stor2 = Store('"СтройДом"','Санкт-Петербург ул.Войнова д.15')
 print(stor2.name_store,stor2.address)
 stor2.add_items('гвозди', 20)
 stor2.add_items('болты', 25)
@@ -38,7 +47,7 @@ print(stor2.get_price('болты'))
 stor2.update_price('гвозди', 30)
 print(stor2.get_price('гвозди'))
 
-stor3 = Store('"Стиль"','Санкт-Петербург,ул.Халтурина,д.27')
+stor3 = Store('"Стиль"','Санкт-Петербург ул.Халтурина д.27')
 print(stor3.name_store,stor3.address)
 stor3.add_items('носки', 200)
 stor3.add_items('шнурки', 70)
